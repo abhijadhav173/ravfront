9ec3d76720bbfc840074faedd28d45abda3136dd
Root4tech <usman@root4tech.com>
2026-02-13 11:40:50 +0500

    Api Path Fixing

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // output: "export",
   trailingSlash: true,
+  async rewrites() {
+    const base = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") || "https://backend.ravokstudios.com";
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${base}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
