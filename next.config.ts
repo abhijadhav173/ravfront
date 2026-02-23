efe0d4fd1f718bab80e3409b1d9a4c19bb97c31f
Root4tech <usman@root4tech.com>
2026-02-23 13:32:49 +0500

    Update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // output: "export",
   trailingSlash: true,
   async rewrites() {
-    const base = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") || "http://localhost:8000";
+    const base = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") || "https://backend.ravokstudios.com";
     return [
       {
         source: "/api/:path*",
